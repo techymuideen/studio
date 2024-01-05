8323623a22eca1b3b28d4857e763b991c0928321
Muideen Popoola <popoolamuideen61@gmail.com>
2024-01-05 21:08:48 +0100

    Added the projects page children routes

--- src/features/projects/Projects.jsx
+++ src/features/projects/Projects.jsx
@@ -1,8 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 const Projects = () => {
   return (
     <div>
       This is where you will get to see all the exciting projects i am working
       on
+      <NavLink to="/projects">Web Projects</NavLink>
+      <NavLink to="/projects/technologies">Technologies</NavLink>
+      <NavLink to="/projects/myprocess">My Process</NavLink>
     </div>
   );
 };
