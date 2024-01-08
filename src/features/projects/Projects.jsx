010434fd486cef6ca698d151f91c0790f3ee2600
Muideen Popoola <popoolamuideen61@gmail.com>
2024-01-08 20:59:26 +0100

    added the project sublink

--- src/features/projects/Projects.jsx
+++ src/features/projects/Projects.jsx
@@ -5,19 +5,50 @@ import Card from "../../ui/Card";
 const Projects = () => {
   return (
     <div className="">
-      <div className="flex py-20 pl-4 dark:bg-black">
+      <div className="flex pb-10 pl-4  pt-20 dark:bg-black">
         <div className="flex-[50%]">
           <Card />
         </div>
         <div className="flex-[50%] bg-red-500">hgdjafghj</div>
       </div>
-      <div>
-        <NavLink to="/projects">Web Projects</NavLink>
-        <NavLink to="/projects/technologies">Technologies</NavLink>
-        <NavLink to="/projects/myprocess">My Process</NavLink>
-      </div>
-      <div>
-        <Outlet />
+      <div className="h-[10px] bg-gradient-to-r from-pink to-[#9C27B0]" />
+      <div className="bg-[#F5F5F5] px-4 py-8 dark:bg-black">
+        <div className="flex ">
+          <NavLink
+            className={(navData) =>
+              navData.isActive
+                ? " flex-1 border-b-4 border-pink bg-[#fff] py-3 text-center font-medium uppercase text-pink hover:bg-lightpink dark:bg-neum-bg-dark hover:dark:bg-lightpink-dark "
+                : " flex-1 bg-[#fff] py-3 text-center font-medium uppercase hover:bg-ash dark:bg-neum-bg-dark dark:text-ash dark:hover:bg-[rgb(45,45,45)]"
+            }
+            to="/projects"
+            end
+          >
+            Web Projects
+          </NavLink>
+          <NavLink
+            className={(navData) =>
+              navData.isActive
+                ? " flex-1 border-b-4 border-pink bg-[#fff] py-3 text-center font-medium uppercase text-pink hover:bg-lightpink dark:bg-neum-bg-dark hover:dark:bg-lightpink-dark"
+                : " flex-1 bg-[#fff] py-3 text-center font-medium uppercase hover:bg-ash dark:bg-neum-bg-dark dark:text-ash dark:hover:bg-[rgb(45,45,45)]"
+            }
+            to="/projects/technologies"
+          >
+            Technologies
+          </NavLink>
+          <NavLink
+            className={(navData) =>
+              navData.isActive
+                ? "u  h flex-1 border-b-4 border-pink bg-[#fff] py-3 text-center font-medium uppercase text-pink hover:bg-lightpink dark:bg-neum-bg-dark hover:dark:bg-lightpink-dark"
+                : "flex-1 bg-[#fff] py-3 text-center font-medium uppercase hover:bg-[#f1f1f1] dark:bg-neum-bg-dark dark:text-ash dark:hover:bg-[rgb(45,45,45)]"
+            }
+            to="/projects/myprocess"
+          >
+            My Process
+          </NavLink>
+        </div>
+        <div className="pt-10">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
